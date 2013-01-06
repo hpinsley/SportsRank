3b928386aed6132b93c1ba1a5767cce8abc357f2
Howard Pinsley <hpinsley@gmail.com>
2013-01-06 18:09:34 -0500

    Add team details -- analyzing the grade computation.

--- TeamGrade.Web/Controllers/TeamController.cs
+++ TeamGrade.Web/Controllers/TeamController.cs
@@ -15,99 +15,18 @@ namespace TeamGrade.Web.Controllers
             _teamManager = teamManager;
         }
 
-        //
-        // GET: /Team/
-
         public ActionResult Index() {
             IEnumerable<Team> teams = _teamManager.GetTeams();
             ViewBag.Title = "2012 NFL Regular Season Grades";
             return View(teams);
         }
 
-        //
-        // GET: /Team/Details/5
-
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        //
-        // GET: /Team/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Team/Create
-
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Team/Edit/5
-
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Team/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Team/Delete/5
-
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Team/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+        public ActionResult Details(string teamName) {
+            Team team = _teamManager.GetTeam(teamName);
+            TeamAnalysis analysis = Grader.GetTeamGradeAnalysis(team);
+            IEnumerable<TeamGameAnalysis> teamGameAnalyses = analysis.GetGameAnalyses();
+            ViewBag.Team = team;
+            return View(teamGameAnalyses);
         }
     }
 }
